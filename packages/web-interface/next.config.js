80db6da3c4fb6b3fe688b2e23569ce789e18052d
James Martin <james.martin@monster.com>
2023-04-25 11:36:32 -0400

    build: remove next config changes

--- packages/web-interface/next.config.js
+++ packages/web-interface/next.config.js
@@ -1,25 +1,11 @@
 // @ts-check
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '';
-let basePath = '/';
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
 /**
  * @type {import('next').NextConfig}
  **/
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
     unoptimized: true,
   },
