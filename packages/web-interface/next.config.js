29edda6d4b6a88fc31a9d6b71135ad666112dcf8
James Martin <james.martin@monster.com>
2023-04-25 10:45:01 -0400

    build: adding github pages scripts

--- packages/web-interface/next.config.js
+++ packages/web-interface/next.config.js
@@ -1,11 +1,25 @@
 // @ts-check
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '/';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 /**
  * @type {import('next').NextConfig}
  **/
 
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
